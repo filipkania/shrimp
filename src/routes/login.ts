481010ef809039e3b742480779468334f42853d5
Filip Kania <fkania2@gmail.com>
2023-09-28 18:22:47 +0200

    feat: add support for logging in

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,4 +1,3 @@
-// import { drizzle } from "drizzle-orm/d1";
 import { AppContext } from "..";
 import { SignJWT } from "jose";
 
@@ -7,43 +6,25 @@ import { arrayBufferToHex } from "../utils/arrayBufferToHex";
 export const method = "POST";
 export const route = "/login";
 
+type User = {
+	id: number;
+	username: string;
+	password: string;
+	salt: string;
+};
+
 export const handler = async (c: AppContext) => {
-	return;
-	const db = drizzle(c.env.DB, { schema });
 	const { username, password } = await c.req.json();
+	if (!username || !password) return c.json({ message: "Invalid username or password." }, 401);
 
-	if (!username || !password)
-		return c.json(
-			{
-				message: "Invalid username or password.",
-			},
-			401
-		);
+	const user: User | null = await c.env.DB.prepare("SELECT * FROM users WHERE username = ?;")
+		.bind(username.toString().toLowerCase())
+		.first();
 
-	const user = await db.query.users.findFirst({
-		where: eq(schema.users.username, username.toString()),
-	});
-	if (!user)
-		return c.json(
-			{
-				message: "Invalid username or password.",
-			},
-			401
-		);
+	if (!user) return c.json({ message: "Invalid username or password." }, 401);
 
-	const passwdDigest = await crypto.subtle.digest(
-		{
-			name: "SHA-256",
-		},
-		new TextEncoder().encode(password.toString() + user.salt)
-	);
-	if (arrayBufferToHex(passwdDigest) !== user.password)
-		return c.json(
-			{
-				message: "Invalid username or password.",
-			},
-			401
-		);
+	const passwdDigest = await crypto.subtle.digest("SHA-256", Buffer.from(password.toString() + user.salt));
+	if (arrayBufferToHex(passwdDigest) !== user.password) return c.json({ message: "Invalid username or password." }, 401);
 
 	const token = await new SignJWT({
 		id: user.id,
@@ -53,7 +34,7 @@ export const handler = async (c: AppContext) => {
 		.setIssuedAt()
 		.setIssuer("urn:shrimp:issuer")
 		.setExpirationTime("2d")
-		.sign(new Uint8Array(new TextEncoder().encode(c.env.JWT_SECRET) as ArrayBuffer));
+		.sign(Buffer.from(c.env.JWT_SECRET));
 
 	return c.json({
 		token,
