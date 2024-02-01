6fc36ee072d135bdbc13a59970bb3dc1aab8a274
Filip Kania <fkania2@gmail.com>
2024-02-01 22:48:15 +0100

    feat: add logout button

--- client/components/inbox/inbox-nav.tsx
+++ client/components/inbox/inbox-nav.tsx
@@ -1,6 +1,7 @@
 import {
   AlertCircleIcon,
   InboxIcon,
+  LogOutIcon,
   MailIcon,
   NotebookPenIcon,
   SendIcon,
@@ -11,8 +12,19 @@ import {
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "../ui/dropdown-menu";
+import { useAuth } from "@/lib/auth/AuthContext";
 
 export const InboxNav = () => {
+  const { logout } = useAuth();
+
   return (
     <nav className="flex flex-col justify-between w-[16rem] h-full border-r">
       <div className="flex flex-col">
@@ -114,17 +126,32 @@ export const InboxNav = () => {
         </Button>
       </div>
 
-      <div className="w-full border-t z-1 px-6 py-4 flex gap-3 hover:bg-muted cursor-pointer transition-colors">
-        <Avatar className="w-10 h-10">
-          <AvatarImage src="https://github.com/filipkania.png" />
-          <AvatarFallback delayMs={500}>FK</AvatarFallback>
-        </Avatar>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-full border-t z-1 px-6 py-4 flex gap-3 hover:bg-muted cursor-pointer transition-colors">
+          <Avatar className="w-10 h-10">
+            <AvatarImage src="https://github.com/filipkania.png" />
+            <AvatarFallback delayMs={500}>FK</AvatarFallback>
+          </Avatar>
 
-        <div className="flex flex-col justify-center">
-          <span className="text-sm font-medium leading-4">Filip Kania</span>
-          <span className="text-muted-foreground text-xs">fkrq.xyz</span>
-        </div>
-      </div>
+          <div className="flex flex-col justify-center items-start">
+            <span className="text-sm font-medium leading-4">Filip Kania</span>
+            <span className="text-muted-foreground text-xs">fkrq.xyz</span>
+          </div>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+            onClick={() => logout()}
+            className="cursor-pointer min-w-[10rem] bg-red-100/40 text-red-500 hover:!bg-red-100/80 hover:!text-red-600"
+          >
+            <LogOutIcon className="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   );
 };
