2d915b9d65ebf059df3fe3c7b581b4dc4dbe336e
Filip Kania <fkania2@gmail.com>
2024-02-02 17:18:55 +0100

    fix: fix gray bar

--- client/components/ui/drawer.tsx
+++ client/components/ui/drawer.tsx
@@ -41,12 +41,12 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 pb-6 flex flex-col h-[90%] rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 pb-6 flex flex-col h-[95%] rounded-t-[10px] border bg-background",
         className
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-[5px] w-[50px] rounded-full bg-black" />
+      <div className="mx-auto mt-4 w-[50px] rounded-full bg-muted" style={{ height: "5px" }}>&nbsp;</div>
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
