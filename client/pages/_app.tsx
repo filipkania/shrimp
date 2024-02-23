411ca8208f9462814352dc2684a6eea366f39137
Filip Kania <fkania2@gmail.com>
2024-02-23 21:42:28 +0100

    feat: add no-referrer policy to not leak Shrimp's domain

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         />
+        <meta name="referrer" content="no-referrer" />
       </Head>
       <QueryClientProvider client={queryClient}>
         <CSRAuthProvider>
