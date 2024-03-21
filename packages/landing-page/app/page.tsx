75aa246a1db4991d6a66ef8ddbccc204fbcb4be5
Filip Kania <fkania2@gmail.com>
2024-03-21 18:27:47 +0100

    feat(landing-page): smaller badge font on mobile

--- packages/landing-page/app/page.tsx
+++ packages/landing-page/app/page.tsx
@@ -10,18 +10,18 @@ const Home = () => {
       <main className="m-auto flex flex-col items-center justify-evenly overflow-hidden py-8 lg:py-16">
         <div className="my-10 flex flex-col items-center gap-3 px-16 text-center">
           <span className="text-6xl font-bold lg:text-7xl">Shrimp</span>
-          <span className="text-xl opacity-50">
+          <span className="text-lg opacity-50 lg:text-xl">
             Open-source, fully encrypted & self-hostable email service.
           </span>
 
-          <span className="mt-6 rounded-full bg-green-300/60 px-6 py-2">
-            Coming soon to your nearest cluster&#8482;
+          <span className="lg:text-md mt-6 rounded-full bg-green-300/60 px-6 py-2 text-sm">
+            Coming soon to your nearest Docker&#8482;
           </span>
         </div>
 
         <div className="relative m-8 flex max-w-[2148px] justify-center lg:px-24">
           <div className="absolute -z-10 h-[100%] w-[100%] scale-[2] bg-gradient-radial from-white via-transparent via-60% to-transparent opacity-20"></div>
-          <Image src={LandingImage} alt="" className="w-[100vw] " />
+          <Image src={LandingImage} unoptimized alt="" className="w-[100vw] " />
         </div>
 
         <div className="flex gap-4">
