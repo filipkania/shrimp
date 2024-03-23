e95c94340e017786272c37016934a44b1f006292
Filip Kania <fkania2@gmail.com>
2024-03-23 18:37:09 +0100

    chore(landing-page): use proper tags

--- packages/landing-page/app/page.tsx
+++ packages/landing-page/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import LandingImage from "../public/landing-image.png";
 
 const LINK_STYLES = "opacity-30 transition-opacity hover:opacity-100";
@@ -7,12 +6,12 @@ const LINK_STYLES = "opacity-30 transition-opacity hover:opacity-100";
 const Home = () => {
   return (
     <>
-      <main className="m-auto flex flex-col items-center justify-evenly overflow-hidden py-8 lg:py-16">
-        <div className="my-10 flex flex-col items-center gap-3 px-16 text-center">
-          <span className="text-6xl font-bold lg:text-7xl">Shrimp</span>
-          <span className="text-lg opacity-50 lg:text-xl">
+      <main className="m-auto flex h-auto flex-col items-center justify-evenly overflow-hidden py-8 lg:py-16">
+        <div className="my-6 flex flex-col items-center gap-3 px-16 text-center lg:my-10">
+          <h1 className="text-5xl font-bold lg:text-7xl">Shrimp</h1>
+          <h4 className="text-md opacity-50 lg:text-xl">
             Open-source, fully encrypted & self-hostable email service.
-          </span>
+          </h4>
 
           <span className="lg:text-md mt-6 rounded-full bg-green-300/60 px-6 py-2 text-sm">
             Coming soon to your nearest Docker&#8482;
@@ -20,7 +19,7 @@ const Home = () => {
         </div>
 
         <div className="relative m-8 flex max-w-[2148px] justify-center lg:px-24">
-          <div className="absolute -z-10 h-[100%] w-[100%] scale-[2] bg-gradient-radial from-white via-transparent via-60% to-transparent opacity-20"></div>
+          <div className="absolute -z-10 h-[100%] w-[100%] scale-[2.5] bg-gradient-radial from-white via-transparent via-60% to-transparent opacity-20 lg:scale-[2]"></div>
           <Image src={LandingImage} unoptimized alt="" className="w-[100vw] " />
         </div>
 
